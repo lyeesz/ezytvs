348b9c89b7674a74f4092c241f164d5d2755f4ed
lyeesz <122807295+lyeesz@users.noreply.github.com>
2023-01-16 20:48:39 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "ilyasszahfir22@gmail.com.com";
+    $email_to = "ilyasszahfir22@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
